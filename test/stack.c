2a98c78a298eb49f96874b148ea500169524e2fa
JCPG <nounpatroon@gmail.com>
2023-03-05 16:39:48 +0200

    Adding "strings" to stack example in c89

--- test/stack.c
+++ test/stack.c
@@ -4,6 +4,7 @@
 #include "../source/stack.h"
 
 int main() {
+  /*Define*/
   typedef struct {
     int x;
     float y;
@@ -11,34 +12,50 @@ int main() {
   
   int i;
   stack_st *vertexes; vex_st *vertexes_;
+  stack_st *strings; char *strings_;
 
+  /*Alloc*/
   stack_alloc(&vertexes, (char**)&vertexes_, sizeof(vex_st));
+  stack_alloc(&strings, (char**)&strings_, sizeof(char) * (12 + 1)); /*one more character for end string '\0'*/
 
-  for(i = 0;i < 5;i++) {
-    vertexes_->x = i+1; vertexes_->y = i+i;
-    stack_push(vertexes, (char*)vertexes_);
-  }
+  /*Push and clear*/
+  vertexes_->x = 10; vertexes_->y = 3.3;
+  stack_push(vertexes, (char*)vertexes_);
   for(i = 0;i < 10;i++) {
-    stack_pop(vertexes);
+    stack_clear(vertexes);
   }
-  stack_clear(vertexes);
-  for(i = 0;i < 5;i++) {
-    vertexes_->x = i+1; vertexes_->y = i+i;
-    stack_push(vertexes, (char*)vertexes_);
-  }
-  stack_clear(vertexes);
-  for(i = 0;i < 5;i++) {
-    vertexes_->x = i+1; vertexes_->y = (float)i + 1.25;
-    stack_push(vertexes, (char*)vertexes_);
-  }
+  vertexes_->x = 42; vertexes_->y = 8.24;
+  stack_push(vertexes, (char*)vertexes_);
+  vertexes_->x = 34; vertexes_->y = 6.55;
+  stack_push(vertexes, (char*)vertexes_);
 
+  for(i = 0;i < 10;i++) {
+    stack_clear(strings);
+  }
+  sprintf(strings_, "%-12.12s", "Hello world!");
+  stack_push(strings, (char*)strings_);
+  sprintf(strings_, "%-12.12s", "Knee-Deep in the dead");
+  stack_push(strings, (char*)strings_);
+  sprintf(strings_, "%-12.12s", "Bye world");
+  stack_push(strings, (char*)strings_);
+
+  /*Pop and top*/
   stack_pop(vertexes);
   stack_top(vertexes, (char*)vertexes_);
+
+  stack_pop(strings);
+  stack_top(strings, (char*)strings_);
   
+  /*Print*/
   printf("vertexes total items: %d\r\n", stack_lenght(vertexes));
   printf("vertexes top: x:%d y:%f\r\n", vertexes_->x, vertexes_->y);
 
+  printf("strings total items: %d\r\n", stack_lenght(strings));
+  printf("strings top: %s\r\n", strings_);
+
+  /*Free*/
   stack_free(&vertexes, (char**)&vertexes_);
+  stack_free(&strings, (char**)&strings_);
 
   if((!vertexes) && (!vertexes_)) {
     printf("Yes, they are null, %p %p\r\n", vertexes, vertexes_);
